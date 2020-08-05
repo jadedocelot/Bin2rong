409539ec11778abf5115dc8454789a068d03763b
bin2rong <eabelortega@gmail.com>
2020-08-05 15:48:54 -0700

    yea

---
+++ python/free_Code_CAmp/Random thunks/understanding_for.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+# friends = ['Joe','Meg','Alex'] ## Old set
+friends = ['Joe','Kim','Alex','Maria','Meg','Edgar','Bryan'] ## New set
+
+#for friend in friends: # The (for) statement will be assigned (and will print) to each element within the set 
+#print('Happy New Year ', friend)
+
+for friend in friends:
+	print('Happy New Year ', friend)
+	# if friend == 'Meg': # This will have the iteration stop and break at 'Meg'
+	#	break
+	#else: # if Meg is never reached then the iteration will continue
+	#	continue 
+print("That's the end of our set.")
