b2ad498d407ed1001de7ffc627f68b52bbd7bf88
Edgar <jadedocelot@gmail.com>
2020-08-26 18:08:22 -0700

    Setting up the Linux station

--- C++/index/hello_world/exceptions.cpp
+++ C++/index/hello_world/exceptions.cpp
@@ -4,3 +4,7 @@
 - When executing C++ code, different errors can occur: coding errors made by the programmer, errors dude wrong input, or other unforesceeable things.
 
 // Is this similar to the try/except
+- When an error occurs, C++ will normally stop and generate an error message. The technical term for this is: C++ will throw an exception (throw an error)
+
+
+	C++ try and catch 
