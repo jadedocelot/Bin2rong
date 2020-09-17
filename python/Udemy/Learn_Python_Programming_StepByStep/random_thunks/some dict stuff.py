a7e252b4348d8a49259a588079a179aad31672d6
bin2rong <eabelortega@gmail.com>
2020-09-17 00:38:09 -0700

    py

---
+++ python/Udemy/Learn_Python_Programming_StepByStep/random_thunks/some dict stuff.py
@@ -0,0 +1,19 @@
+
+mathcedColors = {1:"Blood",2:"Sadness",3:"joy",4:"summer"}
+
+print(mathcedColors)
+# {1: 'Blood', 2: 'Sadness', 3: 'joy', 4: 'summer'}
+
+
+
+print(mathcedColors[3])
+# joy
+print(mathcedColors.keys())
+
+print(mathcedColors.items())
+
+# for laser in mathcedColors.values():
+# 	print(laser, end="•")
+
+for k,l in mathcedColors.items():
+	print(k,"º",l)
