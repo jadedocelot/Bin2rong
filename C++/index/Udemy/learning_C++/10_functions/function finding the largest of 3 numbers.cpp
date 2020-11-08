156bedd329ec5f6f89ff419039de151236b078ae
bin2rong <eabelortega@gmail.com>
2020-11-07 17:54:27 -0800

    BOOKS

--- C++/index/Udemy/learning_C++/10_functions/function finding the largest of 3 numbers.cpp
+++ C++/index/Udemy/learning_C++/10_functions/function finding the largest of 3 numbers.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+// big_boi is our function
 int big_boi(int a,int b,int c) {
     if (a > b && a > c){
         return a;
@@ -21,9 +21,9 @@ fp = big_boi;
 
 cout << "Enter a number for x " << endl; 
 cin >> x;
-cout << "Enter a number for x " << endl;
+cout << "Enter a number for y " << endl;
 cin >> y;
-cout << "Enter a number for x " << endl;
+cout << "Enter a number for z " << endl;
 cin >> z;
 
 cout << "And the one with the biggest dick in the room is: " << (fp)(x,y,z) << endl;
--- python/Udemy/Learn_Python_Programming_StepByStep/16_Database_SQLite/database/deleting records.py
+++ python/Udemy/Learn_Python_Programming_StepByStep/16_Database_SQLite/database/deleting records.py
@@ -1,1 +1,15 @@
-q
+import sqlite3
+
+con = sqlite3.connect("mydatabase.db")
+
+try:
+    cur = con.cursor()
+
+    cur.execute("DELETE FROM student WHERE name = 'poop'")
+    # DELETE FROM <table name> WHERE <column name> = <value>
+    # NOTE: WHERE clause is VERY IMPORTANT, for if condition is not specified, all records from studeb table will be deleted
+    con.commit()
+except Exception as err:
+    print(err)
+finally:
+    con.close()
