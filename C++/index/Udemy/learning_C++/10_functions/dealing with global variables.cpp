311fdd71d487afb54540ed295c7b3d8bf26023d9
0rt <jadedocelot@gmail.com>
2020-11-10 21:34:09 -0800

    dsds

--- C++/index/Udemy/learning_C++/10_functions/dealing with global variables.cpp
+++ C++/index/Udemy/learning_C++/10_functions/dealing with global variables.cpp
@@ -6,11 +6,16 @@ using namespace std;
 // X will be a global variable
 //Global variables reside outside of the the main and sub functions(They reside outside of all the functions)
 // global variables can be accessed by any funcntion
+// As long as the program is running it will always be there in the memory
+// global variables arent stored in the STACK or HEAP memory  but in the CODE memory (SUB section of code section)
 int x = 0;
 
 // local variables withing subfuncntions
+
 // Sub function
+// "func" function goes into sub section of stack memory
 int func(){
+	// Local variable
 	int a =10;
 	x = x + a;
 	for (int i = 0; i < 10;i++){
