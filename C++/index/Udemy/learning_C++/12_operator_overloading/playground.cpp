a95ddf833b142b52855dcfe951437e197b14195e
bin2rong <eabelortega@gmail.com>
2020-12-17 09:17:02 -0800

    ds

--- C++/index/Udemy/learning_C++/12_operator_overloading/playground.cpp
+++ C++/index/Udemy/learning_C++/12_operator_overloading/playground.cpp
@@ -10,7 +10,6 @@ private:
     int img;
 
 public:
-    // The executions included in the outside complex function are also linked to this function
     // FFFFRRRIIIEEENNNDDDSSS 
     complex(int r=0,int i=0)
     {
@@ -23,12 +22,13 @@ public:
         cout << real << " + i" << img;
     }
 
-    friend complex operator+(complex c1,complex c2);
+    // The executions included in the outside complex function are also linked to this function
+    friend complex operator-(complex c1,complex c2);
 };
     // This acts as an independent function.
     // So this does not belong the class above but is a "friend" of it
     // Global Function
-    complex operator+(complex c1,complex c2)
+    complex operator-(complex c1,complex c2)
     {
         complex t;
         t.real = c1.real + c2.real;
@@ -41,7 +41,7 @@ int main()
 {
 
 complex c1(13,5), c2(10,5),c3;
-c3 = operator+(c1,c2);
+c3 = operator-(c1,c2);
 
 c3.display();
 
