d3f27c71548c5dbb49ba038ce60ce6d6954aa7c6
bin2rong <eabelortega@gmail.com>
2020-12-21 22:49:26 -0800

    ds

--- joshua_tree/my_first_neural_network/tensorflow/classify images of clothing.py
+++ joshua_tree/my_first_neural_network/tensorflow/classify images of clothing.py
@@ -94,36 +94,121 @@ model.compile(optimizer='adam',
 # ANCHOR - Metrics:
 # Used to monitor the training and testing steps. The following example uses accuracy, the fraction of the images that are correctly classified
 
-model.fit(train_images,train_labels,epochs=10)
+model.fit(train_images,train_labels,epochs=35)
 
 # To start training, call the ((model.fit)(train_images, train_labels, epochs=10) method - so called because it "fits" the model to the training data:
 
+
+# Test performed on 12/21 10:39 PM 
+# Epoch:10
 '''
+lothing.py
 60000
 [9 0 0 ... 3 0 5]
-2020-12-21 17:33:34.406668: I tensorflow/compiler/xla/service/service.cc:168] XLA service 0x7f823722dd90 initialized for platform Host (this does not guarantee that XLA will be used). Devices:
-2020-12-21 17:33:34.406703: I tensorflow/compiler/xla/service/service.cc:176]   StreamExecutor device (0): Host, Default Version
+2020-12-21 22:37:45.803077: I tensorflow/compiler/xla/service/service.cc:168] XLA service 0x7fad3837b9a0 initialized for platform Host (this does not guarantee that XLA will be used). Devices:
+2020-12-21 22:37:45.803108: I tensorflow/compiler/xla/service/service.cc:176]   StreamExecutor device (0): Host, Default Version
 Epoch 1/10
-1875/1875 [==============================] - 3s 2ms/step - loss: 0.4968 - accuracy: 0.8251
+1875/1875 [==============================] - 4s 2ms/step - loss: 0.4986 - accuracy: 0.8232
 Epoch 2/10
-1875/1875 [==============================] - 3s 2ms/step - loss: 0.3763 - accuracy: 0.8648
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.3745 - accuracy: 0.8651
 Epoch 3/10
-1875/1875 [==============================] - 3s 2ms/step - loss: 0.3343 - accuracy: 0.8796
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.3339 - accuracy: 0.8770
 Epoch 4/10
-1875/1875 [==============================] - 3s 2ms/step - loss: 0.3137 - accuracy: 0.8852
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.3107 - accuracy: 0.8847
 Epoch 5/10
-1875/1875 [==============================] - 3s 1ms/step - loss: 0.2956 - accuracy: 0.8907
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.2903 - accuracy: 0.8930
 Epoch 6/10
-1875/1875 [==============================] - 3s 2ms/step - loss: 0.2806 - accuracy: 0.8958
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.2764 - accuracy: 0.8975
 Epoch 7/10
-1875/1875 [==============================] - 3s 1ms/step - loss: 0.2697 - accuracy: 0.9000
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.2642 - accuracy: 0.9012
 Epoch 8/10
-1875/1875 [==============================] - 3s 2ms/step - loss: 0.2585 - accuracy: 0.9039
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.2551 - accuracy: 0.9043
 Epoch 9/10
-1875/1875 [==============================] - 3s 2ms/step - loss: 0.2486 - accuracy: 0.9079
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.2452 - accuracy: 0.9079
 Epoch 10/10
-1875/1875 [==============================] - 3s 2ms/step - loss: 0.2395 - accuracy: 0.9103
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.2369 - accuracy: 0.9117
+313/313 - 0s - loss: 0.3400 - accuracy: 0.8816
+
+Test accuracy:  0.881600022315979
 '''
+# Test performed on 12/21 10:48 PM 
+# Epoch:35
+'''
+Epoch 1/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.4979 - accuracy: 0.8251
+Epoch 2/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.3736 - accuracy: 0.8659
+Epoch 3/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.3349 - accuracy: 0.8784
+Epoch 4/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.3111 - accuracy: 0.8866
+Epoch 5/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.2962 - accuracy: 0.8908
+Epoch 6/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.2806 - accuracy: 0.8965
+Epoch 7/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.2675 - accuracy: 0.8994
+Epoch 8/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.2585 - accuracy: 0.9038
+Epoch 9/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.2474 - accuracy: 0.9080
+Epoch 10/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.2399 - accuracy: 0.9099
+Epoch 11/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.2316 - accuracy: 0.9129
+Epoch 12/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.2224 - accuracy: 0.9175
+Epoch 13/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.2151 - accuracy: 0.9192
+Epoch 14/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.2117 - accuracy: 0.9204
+Epoch 15/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.2043 - accuracy: 0.9237
+Epoch 16/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1978 - accuracy: 0.9256
+Epoch 17/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1915 - accuracy: 0.9295
+Epoch 18/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1879 - accuracy: 0.9288
+Epoch 19/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1816 - accuracy: 0.9320
+Epoch 20/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1795 - accuracy: 0.9327
+Epoch 21/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1746 - accuracy: 0.9344
+Epoch 22/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1706 - accuracy: 0.9354
+Epoch 23/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1663 - accuracy: 0.9377
+Epoch 24/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1625 - accuracy: 0.9392
+Epoch 25/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1568 - accuracy: 0.9411
+Epoch 26/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1540 - accuracy: 0.9419
+Epoch 27/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1524 - accuracy: 0.9424
+Epoch 28/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1473 - accuracy: 0.9441
+Epoch 29/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1431 - accuracy: 0.9456
+Epoch 30/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1409 - accuracy: 0.9477
+Epoch 31/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1388 - accuracy: 0.9473
+Epoch 32/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1357 - accuracy: 0.9494
+Epoch 33/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1349 - accuracy: 0.9494
+Epoch 34/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1301 - accuracy: 0.9512
+Epoch 35/35
+1875/1875 [==============================] - 3s 2ms/step - loss: 0.1300 - accuracy: 0.9513
+313/313 - 0s - loss: 0.4127 - accuracy: 0.8897
+
+Test accuracy:  0.8896999955177307
+'''
+
 # As the model trains, the loss and accuracy metrics are displayed. This model reaches an accuracy of about 0.91 (or 91%) on  the training data
 
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
