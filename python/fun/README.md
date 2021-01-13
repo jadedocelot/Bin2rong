4fdbfcbe1328bfd3a9ae106d3cff92df25db31c4
bin2rong <eabelortega@gmail.com>
2021-01-13 12:15:41 -0800

    arrays

---
+++ python/fun/README.md
@@ -0,0 +1,4 @@
+# TO DO
+
+# Working On
+    -  Working on a tower of arrays
--- python/fun/tower_of_numbers.py
+++ python/fun/tower_of_numbers.py
@@ -1,1 +1,19 @@
-im
+import numpy as np
+import numpy.matlib
+import matplotlib.pyplot as plt
+
+x_tower = np.arange(50).reshape(5,5,2)
+y_tower = np.arange(50).reshape(10,5)
+
+# sprint("Tower X")
+# sprint(x_tower)
+# sprint("t\∂Tower Y")
+# sprint(y_tower)
+
+for i in x_tower:
+	for j in i:
+		for x in j:
+			if x != 28:
+				print(x)
+			else:
+				print("Code RED. WE ARE SO SMART!")
