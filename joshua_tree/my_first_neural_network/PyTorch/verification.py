d85ff14caff67a8d48b51e8ae24655495a1c9a69
0rt <jadedocelot@gmail.com>
2021-02-02 11:22:31 -0600

    Heskell

--- joshua_tree/my_first_neural_network/PyTorch/verification.py
+++ joshua_tree/my_first_neural_network/PyTorch/verification.py
@@ -2,6 +2,7 @@ import torch
 x = torch.rand(5, 3)
 # y should create a tensor of 0s
 y = torch.Tensor(3, 5)	
+d_add = torch.tensor(some_list, device=device)
 d_count = torch.cuda.device_count()
 d_name = torch.cuda.get_device_name(device_id)
 
