c24ba4c21e6481f65192e5b48a11ed13465d33c7
0rt <jadedocelot@gmail.com>
2021-02-02 09:18:34 -0600

    t

--- joshua_tree/my_first_neural_network/PyTorch/verification.py
+++ joshua_tree/my_first_neural_network/PyTorch/verification.py
@@ -2,8 +2,13 @@ import torch
 x = torch.rand(5, 3)
 # y should create a tensor of 0s
 y = torch.Tensor(3, 5)	
+d_count = torch.cuda.device_count()
+d_name = torch.cuda.get_device_name(device_id)
 
 # ONE tensor
 r = torch.ones (5,5) * 5
 
-print(r)
+# print(r)
+# print(x)
+print(d_name)
+print(d_count)
